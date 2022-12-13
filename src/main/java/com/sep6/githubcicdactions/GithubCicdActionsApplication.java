1773a99d2bd3fd49bb9ca99fc42dde21eb13b9b5
satish627 <satish.grg627@gmail.com>
2022-12-13 10:30:04 +0100

    addUsers service added

--- src/main/java/com/sep6/githubcicdactions/GithubCicdActionsApplication.java
+++ src/main/java/com/sep6/githubcicdactions/GithubCicdActionsApplication.java
@@ -3,6 +3,7 @@ package com.sep6.githubcicdactions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
@@ -12,6 +13,11 @@ public class GithubCicdActionsApplication {
 	@GetMapping("/welcome")
 	public String welcome(){
 		return "Welcome to github actions";
+	}
+
+	@PostMapping("/addUsers")
+	public String addUsers(){
+		return "Hello from java";
 	}
 	public static void main(String[] args) {
 		SpringApplication.run(GithubCicdActionsApplication.class, args);
